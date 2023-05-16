53266e54ccbc400b1dab6f4cda2a2ba28ff4ccc6
delyan-kirov <delyan.k.kirov@gmail.com>
2023-05-16 15:42:13 +0300

    Initialize Test

--- Lattice Model/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-conjure solve -ac --number-of-solutions=all boolean2.essence n.param
-#conjure solve boolean.essence n.param
--- Lattice Model/delete.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-find -iname '*.solution' -type f -print0  | xargs --null -n 100 rm -vrf | wc -l
